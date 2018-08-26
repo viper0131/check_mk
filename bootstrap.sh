d44f03f336043778a67725ae51e511d8cb67e6ed
adolci <dolci.alessandro94@gmail.com>
2018-08-26 15:30:35 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@
 if [ ! -d "/opt/omd/sites/${CMK_SITE}" ] ; then
     omd create ${CMK_SITE}
     omd config ${CMK_SITE} set APACHE_TCP_ADDR 0.0.0.0
-    htpasswd -b -m /omd/sites/${CMK_SITE}/etc/htpasswd cmkadmin omd
+    htpasswd -b -m /omd/sites/${CMK_SITE}/etc/htpasswd cmkadmin ${CMK_PASSWORD}
     ln -s "/omd/sites/${CMK_SITE}/var/log/nagios.log" /var/log/nagios.log
     /opt/redirector.sh ${CMK_SITE} > /omd/sites/${CMK_SITE}/var/www/index.html
 fi
