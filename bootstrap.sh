e90dfec7bfce7153a981e83188c3178a0cd550f2
Ilya Vas <e-v1l@yandex.ru>
2018-09-06 16:17:06 +0300

    Add command for enable second use container with using external volume sites
    Principes:
    1. Store tmpfs for CMK_SITE
    2. Second launch restore tmpfs and create user CMK_SITE

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
+
+# Dir for store system files
+STORETC="/opt/omd/etc"
+
 # Init check_mk instance
 if [ ! -d "/opt/omd/sites/${CMK_SITE}" ] ; then
     omd create ${CMK_SITE}
     omd config ${CMK_SITE} set APACHE_TCP_ADDR 0.0.0.0
     htpasswd -b -m /omd/sites/${CMK_SITE}/etc/htpasswd cmkadmin ${CMK_PASSWORD}
-    ln -s "/omd/sites/${CMK_SITE}/var/log/nagios.log" /var/log/nagios.log
     /opt/redirector.sh ${CMK_SITE} > /omd/sites/${CMK_SITE}/var/www/index.html
+    
+    # Check dir and save omd tmpfs for repeated use
+    [ ! -d ${STORETC} ] && \
+	mkdir -p ${STORETC} && \
+	grep omd /etc/fstab > ${STORETC}/fstab
+else
+    # Restore and create settings omd for using with new container
+    useradd -M -c "OMD site ${CMK_SITE}" -b "/omd/sites" -U -G apache
+    cat ${STORETC}/fstab >> /etc/fstab
+    mount tmpfs
+    omd enable ${CMK_SITE}
 fi
+
+
+# This link needs always
+ln -s "/omd/sites/${CMK_SITE}/var/log/nagios.log" /var/log/nagios.log
 
 
 # Create SSMTP config
@@ -28,4 +46,3 @@ chown root:mail $CFGFILE
 
 # Start check_mk
 omd start && tail -f /var/log/nagios.log
-
