659f16d03cab6a859817796206b02334c4138924
Ilya Vas <e-v1l@yandex.ru>
2018-09-07 09:11:08 +0300

    Optimise script, Fix errors

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-
-# Dir for store system files
-STORETC="/opt/omd/etc"
+BASE_OMD="/opt/omd/sites"
 
 # Init check_mk instance
-if [ ! -d "/opt/omd/sites/${CMK_SITE}" ] ; then
+if [ ! -d "${BASE_OMD}/${CMK_SITE}" ] ; then
     omd create ${CMK_SITE}
     omd config ${CMK_SITE} set APACHE_TCP_ADDR 0.0.0.0
     htpasswd -b -m /omd/sites/${CMK_SITE}/etc/htpasswd cmkadmin ${CMK_PASSWORD}
     /opt/redirector.sh ${CMK_SITE} > /omd/sites/${CMK_SITE}/var/www/index.html
     
-    # Check dir and save omd tmpfs for repeated use
-    [ ! -d ${STORETC} ] && \
-	mkdir -p ${STORETC} && \
-	grep omd /etc/fstab > ${STORETC}/fstab
+    # Save omd record about tmpfs for second use
+    grep omd /etc/fstab > ${BASE_OMD}/${CMK_SITE}/.fstab.tmpfs
 else
-    # Restore and create settings omd for using with new container
-    useradd -M -c "OMD site ${CMK_SITE}" -b "/omd/sites" -U -G apache
-    cat ${STORETC}/fstab >> /etc/fstab
+    # Create user and group for using existing site with new container
+    useradd -M -c "OMD site ${CMK_SITE}" -b "/omd/sites" -U -G omd ${CMK_SITE}
+    usermod -aG ${CMK_SITE} apache
+    chown -R ${CMK_SITE}:${CMK_SITE} ${BASE_OMD}/${CMK_SITE}
+    # Restore tmpfs
+    cat ${BASE_OMD}/${CMK_SITE}/.fstab.tmpfs >> /etc/fstab
     mount tmpfs
     omd enable ${CMK_SITE}
 fi
