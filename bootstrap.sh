cc2ff6bd9d6bffc8a11e344d4979266453ebf745
Michael Geiger <info@mgeiger.de>
2018-09-15 12:12:45 +0200

    Small corrections (#36)

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,8 @@ BASE_OMD="/opt/omd/sites"
 
 # Init check_mk instance
 if [ ! -d "${BASE_OMD}/${CMK_SITE}" ] ; then
+
+    # Create new site
     omd create ${CMK_SITE}
     omd config ${CMK_SITE} set APACHE_TCP_ADDR 0.0.0.0
     htpasswd -b -m /omd/sites/${CMK_SITE}/etc/htpasswd cmkadmin ${CMK_PASSWORD}
@@ -11,11 +13,14 @@ if [ ! -d "${BASE_OMD}/${CMK_SITE}" ] ; then
     
     # Save omd record about tmpfs for second use
     grep omd /etc/fstab > ${BASE_OMD}/${CMK_SITE}/.fstab.tmpfs
+
 else
+
     # Create user and group for using existing site with new container
     useradd -M -c "OMD site ${CMK_SITE}" -b "/omd/sites" -U -G omd ${CMK_SITE}
     usermod -aG ${CMK_SITE} apache
     chown -R ${CMK_SITE}:${CMK_SITE} ${BASE_OMD}/${CMK_SITE}
+
     # Restore tmpfs
     cat ${BASE_OMD}/${CMK_SITE}/.fstab.tmpfs >> /etc/fstab
     mount tmpfs
@@ -23,7 +28,7 @@ else
 fi
 
 
-# This link needs always
+# This link is always needed
 ln -s "/omd/sites/${CMK_SITE}/var/log/nagios.log" /var/log/nagios.log
 
 
@@ -45,3 +50,4 @@ chown root:mail $CFGFILE
 
 # Start check_mk
 omd start && tail -f /var/log/nagios.log
+
